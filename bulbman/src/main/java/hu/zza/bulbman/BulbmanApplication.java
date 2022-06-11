f486887803d1a635186be9418923fb8e84ee7df1
Szabó László András <noreply@zza.hu>
2022-06-11 22:25:21 +0200

    init

---
+++ bulbman/src/main/java/hu/zza/bulbman/BulbmanApplication.java
@@ -0,0 +1,13 @@
+package hu.zza.bulbman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulbmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulbmanApplication.class, args);
+	}
+
+}
