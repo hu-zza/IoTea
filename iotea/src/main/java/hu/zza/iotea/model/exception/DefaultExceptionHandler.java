ee83f826480afd0b3a0267ec7c9afb1cd329455d
Szabó László András <noreply@zza.hu>
2022-06-29 22:57:00 +0200

    Exception handler for DB-level violations: unique fields

--- iotea/src/main/java/hu/zza/iotea/model/exception/DefaultExceptionHandler.java
+++ iotea/src/main/java/hu/zza/iotea/model/exception/DefaultExceptionHandler.java
@@ -1,7 +1,31 @@
 package hu.zza.iotea.model.exception;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.zalando.problem.spring.webflux.advice.ProblemHandling;
 
 @ControllerAdvice
-public class DefaultExceptionHandler implements ProblemHandling {}
+public class DefaultExceptionHandler implements ProblemHandling {
+
+  @ExceptionHandler({DataIntegrityViolationException.class})
+  public ResponseEntity<Problem> handleDataIntegrityViolation(
+      DataIntegrityViolationException exception) {
+
+    String messageChain = buildExceptionMessageChain(exception);
+    return ResponseEntity.internalServerError().body(new ServiceProblem(messageChain));
+  }
+
+  private String buildExceptionMessageChain(DataIntegrityViolationException exception) {
+    var msg = exception.getMessage();
+    StringBuilder builder = new StringBuilder(msg == null ? "null" : msg);
+
+    Throwable t = exception;
+
+    while ((t = t.getCause()) != null) {
+      builder.insert(0, "%s >>> ".formatted(t.getMessage()));
+    }
+    return builder.toString();
+  }
+}
